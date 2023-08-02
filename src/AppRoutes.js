01e5477e9fcb88e61b57d593adf1e8f60540f591
Deat1995 <debora2013aguiartavares@gmail.com>
2023-08-02 09:26:50 -0300

    [wip] fix the roots at the app

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,14 +1,16 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
-import Contact from "./pages/Contact/Contact";
+import Contact from "./pages/Contact/Contact"
+import Home from "./pages/Home/Home"
+import About from "./pages/About/About"
 
 
 function AppRoutes() {
     return(
         <>
         <Routes>
-            <Route path="/" element={<>home</>} exact/>
-            <Route path="/sobre" element={<>sobre</>} exact/>
+            <Route path="/" element={<Home/>} exact/>
+            <Route path="/sobre" element={<About/>} exact/>
             <Route path="/contatos" element={<Contact/>} exact/>
         </Routes>
         </>
